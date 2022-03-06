fd157b0294dcfe33efe35fee2e6a3fa4af8aae74
dimaevan <dimaevan@gmail.com>
2022-03-06 12:37:14 +0300

    Edit workflow

--- logger.py
+++ logger.py
@@ -1,7 +1,6 @@
 import logging
 
 LOGGER_FORMAT = '%(asctime)s %(message)s'
-logging.basicConfig(filename="LOG.txt", filemode='w',
-                    format=LOGGER_FORMAT, datefmt='[%H:%M:%S]')
+logging.basicConfig(format=LOGGER_FORMAT, datefmt='[%H:%M:%S]')
 log = logging.getLogger()
 log.setLevel(logging.INFO)
